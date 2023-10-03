ba7d818395c00ca344433f2a74003623a2821998
Mos3aB696 <mos3ab696@gmail.com>
2023-10-03 18:06:41 +0300

    task one (delete n from the end of print)

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:  # use str() to convert int to str and make concat
-    print(str(number) + " is negative\n")
+    print(str(number) + " is negative")
 elif number > 0:  # use placeholder with format
-    print("{} is positive\n".format(number))
+    print("{} is positive".format(number))
 else:
-    print("{} is zero\n".format(number))
+    print("{} is zero".format(number))

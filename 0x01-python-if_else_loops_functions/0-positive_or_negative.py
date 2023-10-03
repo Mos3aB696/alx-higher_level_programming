d52f59d3617735c96dffd16456b37cd710bd75f1
Mos3aB696 <mos3ab696@gmail.com>
2023-10-03 18:13:48 +0300

    task one (add three way to solve task)

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,5 @@ if number < 0:  # use str() to convert int to str and make concat
     print(str(number) + " is negative")
 elif number > 0:  # use placeholder with format
     print("{} is positive".format(number))
-else:
-    print("{} is zero".format(number))
+else:  # use number direct with string (like print val in num and str)
+    print(number, "is zero")

e8cf121dad9e398e8d0d6d456884fd47dd3ffc63
Mos3aB696 <mos3ab696@gmail.com>
2023-10-05 23:37:05 +0300

    task five

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import importlib.util
+import sys
+
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
+
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in sorted(names):
+        print(name)

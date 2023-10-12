78acd2793170bd5c54f74340997133a72660bdad
Mos3aB696 <mos3ab696@gmail.com>
2023-10-12 04:12:25 +0300

    task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
+        dict_values = list(a_dictionary.values())
+        max_val = max(dict_values)
+        for key, val in a_dictionary.items():
+            if val == max_val:
+                return key
     else:
         return None

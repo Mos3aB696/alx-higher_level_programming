35f64fc7e6d8e438b27edbbccf3c88cf97ef9a72
Mos3aB696 <mos3ab696@gmail.com>
2023-10-25 00:43:24 +0300

    task three

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """The init function (constrator)"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

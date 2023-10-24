a29d6945f002da85a4bc500bf59b20f48fec174a
Mos3aB696 <mos3ab696@gmail.com>
2023-10-25 01:56:21 +0300

    task five

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,17 +7,17 @@ class Square:
     """Square class"""
 
     def __init__(self, size=0):
-        """The init function (constrator)"""
+        """(constrator)"""
         self.__size = size
 
     @property
     def size(self):
-        """get_size function"""
+        """get_size method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter function"""
+        """Setter method"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -26,5 +26,5 @@ class Square:
             self.__size = value
 
     def area(self):
-        """function that return current square area"""
+        """method that return current square area"""
         return self.size ** 2

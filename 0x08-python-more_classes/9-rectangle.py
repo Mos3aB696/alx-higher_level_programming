a431a2b0f078c6c86e84bc067c408effc25a7ea5
Mos3aB696 <mos3ab696@gmail.com>
2023-10-31 01:28:10 +0200

    s

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -74,15 +74,18 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) is not Rectangle:
+        if isinstance(rect_1, Rectangle) is False:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
+        elif isinstance(rect_2, Rectangle) is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+        elif Rectangle.area(rect_1) == Rectangle.area(rect_2):
             return rect_1
-        return rect_2
+        elif Rectangle.area(rect_1) > Rectangle.area(rect_2):
+            return rect_1
+        elif Rectangle.area(rect_1) < Rectangle.area(rect_2):
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
-        """Return A New Rectangle"""
+        """returns a new Rectangle instance that is a square w/ h==w==size"""
         return cls(size, size)

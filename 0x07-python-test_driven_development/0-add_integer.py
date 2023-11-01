3afbd6208cb5acf8edc7786492d65022c3b27c78
Mos3aB696 <mos3ab696@gmail.com>
2023-11-01 04:17:24 +0200

    ta0sk

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines The Function That Add Integer"""
+
+
 def add_integer(a, b=98):
+    """Add Integer Function"""
     """Function That Adds Two Number"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")

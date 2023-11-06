97bab425dd427a897ca16a9b360353a916252eda
Mos3aB696 <mos3ab696@gmail.com>
2023-11-07 00:17:57 +0200

    Ta_9_sk

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Create Class"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+
+    def area(self):
+        """Public Instance Method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Check Validation Function"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Inhertance Class"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)

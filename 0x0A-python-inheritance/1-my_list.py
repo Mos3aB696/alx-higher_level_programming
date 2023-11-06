de1bfda2e6b2083cdb4047e2f0346a5c0cf7648a
Mos3aB696 <mos3ab696@gmail.com>
2023-11-06 23:57:48 +0200

    Ta_2sk

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """ subclass of list"""
 
-    def __init__(self):
-        """init the object"""
-        super().__init__()
-
     def print_sorted(self):
         """Sorted Method"""
         print(sorted(self))

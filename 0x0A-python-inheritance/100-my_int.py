36f49268592bbee6ac85411bb1016280c347af5a
Mos3aB696 <mos3ab696@gmail.com>
2023-11-07 01:50:39 +0200

    Ta_1_sk|Advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,8 +5,8 @@
 class MyInt(int):
     def __eq__(self, other):
         """Function That Swap Equal To Not Equal"""
-        return super().__ne__(other)
+        return self.real != other
 
     def __ne__(self, other):
         """Function That Swap Not Equal To Equal"""
-        return super().__eq__(other)
+        return self.real == other

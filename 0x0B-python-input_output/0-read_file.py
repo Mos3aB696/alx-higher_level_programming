c93ab982ebad3296551f8c50c0ec957426def8c8
Mos3aB696 <mos3ab696@gmail.com>
2023-11-07 23:03:24 +0200

    Ta_0_sk

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Function That Read File Content"""
     with open(filename, 'r', encoding="UTF-8") as file:
-        print(file.read())
+        print(file.read(), end="")

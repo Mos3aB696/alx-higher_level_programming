9ff4d8b1be6b523f1ebafb2c4d4e88d03bf5b6f3
Mos3aB696 <mos3ab696@gmail.com>
2023-11-13 23:44:01 +0200

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """str method"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     @property
     def size(self):

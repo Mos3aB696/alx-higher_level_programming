e921895a37d0a895c002e3246641c561b734743b
Mos3aB696 <mos3ab696@gmail.com>
2023-11-13 04:09:51 +0200

    Te_0_st

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
+"""This Is Testing Class"""
 
 
 class TestBase(unittest.TestCase):
+    """Test Base Class"""
+
     def test_if_id_provided(self):
         userOne = Base(12)
         self.assertEqual(userOne.id, 12)
@@ -16,3 +19,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(userOne.id, 1)
         self.assertEqual(userTwo.id, 2)
         self.assertEqual(userThree.id, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()

352b0892e6faf403fb897dd447905bd3babf7bb5
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 01:05:30 +0200

    Ta_6_sk

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def find_peak(lst):
+    n = len(lst)
+    if n == 1:
+        return lst[0]
+    elif n == 2:
+        return max(lst[0], lst[1])
+    elif n == 0:
+        return None
+    mid = n // 2
+    if lst[mid] > lst[mid - 1] and lst[mid] > lst[mid + 1]:
+        return lst[mid]
+    elif lst[mid] <= lst[mid - 1]:
+        return find_peak(lst[:mid])
+    else:
+        return find_peak(lst[mid+1:])

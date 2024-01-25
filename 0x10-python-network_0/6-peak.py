ba32b07b3475555277e20c6ca1419bbb715689af
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 01:25:31 +0200

    Ta_6_sk

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,17 +9,10 @@
 
 
 def find_peak(list_of_integers):
-    n = len(list_of_integers)
-    if n == 1:
-        return list_of_integers[0]
-    elif n == 2:
-        return max(list_of_integers[0], list_of_integers[1])
-    elif n == 0:
-        return None
-    mid = n // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    elif list_of_integers[mid] <= list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid+1:])
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i

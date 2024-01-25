e8e13a912293bec88ee8dc07d19b89c4594c5972
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 01:40:16 +0200

    Ta_6_sk

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,27 +3,25 @@
     The function `find_peak` takes in a list of numbers
     and returns the peak element, which is an
     element that is greater than its adjacent elements.
-    :param list_of_integers: The parameter `list_of_integers`
-    is a list of numbers
+    :param lst: The parameter `lst` is a list of numbers
     :return: the peak element from the given list.
 """
 
 
-def find_peak(list_of_integers):
+def find_peak(lst):
     """BRUTE force implementation for question
     """
-    n = len(list_of_integers)
+    n = len(lst)
     if n == 1:
-        return list_of_integers[0]
+        return lst[0]
     elif n == 2:
-        return max(list_of_integers[0], list_of_integers[1])
+        return max(lst[0], lst[1])
     elif n == 0:
         return None
     mid = n // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and \
-            list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    elif list_of_integers[mid] <= list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    if lst[mid] > lst[mid - 1] and lst[mid] > lst[mid + 1]:
+        return lst[mid]
+    elif lst[mid] <= lst[mid - 1]:
+        return find_peak(lst[:mid])
     else:
-        return find_peak(list_of_integers[mid+1:])
+        return find_peak(lst[mid+1:])

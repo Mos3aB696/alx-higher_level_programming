794afed5873a059edabde902a65e2148bbc7d795
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 00:45:33 +0200

    Ta_5_sk

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#  Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST "$1" -d "email=test@./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""gmail.com&subject=I will always be here for PLD"

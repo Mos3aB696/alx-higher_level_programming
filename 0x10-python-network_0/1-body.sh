9a5602d4a463919c77088f07e458eaa0cb5a3f44
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 00:30:38 +0200

    Ta_1_sk

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL
-response=$(curl -s -w "%{http_code}" "$1"); [ "${response: -3}" -eq 200 ] && echo "${response::-3}"
+curl -sL "$1"

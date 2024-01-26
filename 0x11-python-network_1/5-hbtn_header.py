34e350ba91f0412ef9bbad90103189e440d5a980
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 20:26:47 +0200

    Ta_5_sk

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" The code is importing the `urllib.request` module and the `sys` module.
+"""
+    Python script that takes in a URL, sends a request to the URL and
+    displays the value of the variable X-Request-Id in the response header
 """
 import requests
 import sys
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))

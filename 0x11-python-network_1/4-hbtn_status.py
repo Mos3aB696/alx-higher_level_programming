399eebc0f8780d7d0ddb1324505a65ace6b435e0
Mos3aB696 <mos3ab696@gmail.com>
2024-01-26 20:16:10 +0200

    Ta_4_sk

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,8 +7,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import requests
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    data = response.content
+    data = response.content.decode('utf-8')
     print("Body response:")
     print(f"\t- type: {type(data)}")
     print(f"\t- content: {data}")
-    print(f"\t- utf8 content: {data.decode('utf-8')}")

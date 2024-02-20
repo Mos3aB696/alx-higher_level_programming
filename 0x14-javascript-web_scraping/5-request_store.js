d8ff9da77206d80bbb3646006279a2d6824b8490
Mos3aB696 <mos3ab696@gmail.com>
2024-02-20 22:35:55 +0200

    Ta_5_sk

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
-const request = require("request"),
-  fs = require("fs");
-const url = process.argv[2],
-  path = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
 
 request.get(url, (error, response, body) => {
   if (error) console.log(error);
-  else
-    fs.writeFile(path, body, "utf-8", (err) => {
+  else {
+    fs.writeFile(path, body, 'utf-8', (err) => {
       if (err) console.log(err);
     });
+  }
 });
